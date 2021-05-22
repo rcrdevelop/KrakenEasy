8d95793a4d13aea660beb976b102fa032466b371
Ricardo Contreras Rivero <40666926+RCRDesarrolloweb@users.noreply.github.com>
2021-05-22 15:02:13 +0430

    Actualizacion badges ProgressKraken

--- KrakenEasy/HUDS/ProgressKraken.xaml.cs
+++ KrakenEasy/HUDS/ProgressKraken.xaml.cs
@@ -29,6 +29,7 @@ namespace KrakenEasy.HUDS
         }
         public void Data()
         {
+            var path = Environment.CurrentDirectory;
             MongoAccess _Access = new MongoAccess();
             Application.Current.Dispatcher.Invoke(() =>
             {
@@ -44,7 +45,41 @@ namespace KrakenEasy.HUDS
                         {
                             this.VPIP_Positivo.EndAngle= 270 + _STATS[i] * 2 - 20;
                         }
-                        
+                        if (_STATS[i] >= 0 && _STATS[i] < 10)
+                        {
+                            this.Badge.ImageSource = new BitmapImage(new Uri(path + "/HUDS/Badges/SeaHorse.png"));
+                        }
+                        if (_STATS[i] >= 10 && _STATS[i] < 20)
+                        {
+                            this.Badge.ImageSource = new BitmapImage(new Uri(path + "/HUDS/Badges/Erizo.png"));
+                        }
+                        if (_STATS[i] >= 20 && _STATS[i] < 30)
+                        {
+                            this.Badge.ImageSource = new BitmapImage(new Uri(path + "/HUDS/Badges/Raspa.png"));
+                        }
+                        if (_STATS[i] >= 30 && _STATS[i] < 40)
+                        {
+                            this.Badge.ImageSource = new BitmapImage(new Uri(path + "/HUDS/Badges/Whale.png"));
+                        }
+                        if (_STATS[i] >= 40 && _STATS[i] < 50)
+                        {
+                            this.Badge.ImageSource = new BitmapImage(new Uri(path + "/HUDS/Badges/Erizo.png"));
+                        }
+                        if (_STATS[i] >= 50 && _STATS[i] < 60)
+                        {
+                            this.Badge.ImageSource = new BitmapImage(new Uri(path + "/HUDS/Badges/Worm.png"));
+                        }
+                        if (_STATS[i] >= 60 && _STATS[i] < 75)
+                        {
+                            this.Badge.ImageSource = new BitmapImage(new Uri(path + "/HUDS/Badges/Shark.png"));
+                        }
+                        if (_STATS[i] >= 75)
+                        {
+                            this.Badge.ImageSource = new BitmapImage(new Uri(path + "/HUDS/Badges/pulpo.png"));
+                        }
+
+
+
                     }
                     if (i == 1) // CC
                     {
